8fc56bcabc8065032ea73a3d4ed3f3c8771444c2
ryonaldteofilo <56459761+ryonaldteofilo@users.noreply.github.com>
2019-10-25 15:50:05 +0100

    Added files

---
+++ state_machine.vhd
@@ -0,0 +1,16 @@
+------------------------------------
+      -- state_machine --
+------------------------------------
+
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+library grlib;
+use grlib.amba.all;
+use grlib.stdlib.all;
+use grlib.devices.all;
+library gaisler;
+use gaisler.misc.all;
+library UNISIM;
+use UNISIM.VComponents.all;
+
